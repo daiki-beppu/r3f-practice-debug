febde1096a17e544e47711c9c781448a23fc40e4
daiki-beppu <beppu.engineer@gmail.com>
2026-01-17 13:32:08 +0900

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 
 export const App = () => (
-  <div className="w-dvw h-dvh">
-    <Canvas>
+  <div className="w-dvw h-dvh overflow-hidden">
+    <Canvas> 
       <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
+        <sphereGeometry />
+        <meshNormalMaterial wireframe/>
       </mesh>
     </Canvas>
   </div>
